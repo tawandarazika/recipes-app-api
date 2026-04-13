7e48cecd44ee2b7f8b60eb90301933f274f1346a
TawandaRazika <224015568+tawandarazika@users.noreply.github.com>
2026-04-14 10:38:19 +1200

    Lint error fix v2

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,8 +6,10 @@ Sample Tests for the app.
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Tests for the calc module."""
+
     def test_add_numbers(self):
         """Test adding two numbers together."""
         result = calc.add(5, 3)
